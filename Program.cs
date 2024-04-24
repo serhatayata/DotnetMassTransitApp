31eb0df69984c7a0cb193cde8c6ef7dd9af6e9af
Serhat <srhtayata@gmail.com>
2024-04-24 20:43:11 +0300

    Batch Consumers - Definitions - Skipped Messages

--- Program.cs
+++ Program.cs
@@ -11,19 +11,22 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.Configure<QueueSettings>("QueueSettings", configuration);
 
-builder.Services.AddMassTransit(mt => 
-              mt.AddMassTransit(mts => 
-                 mts.UsingRabbitMq((cntx, cfg) =>
-                 {
-                     var queueSettings = configuration.GetSection("QueueSettings").Get<QueueSettings>();
-                     cfg.Host(queueSettings.Uri, c =>
-                     {
-                         c.Username(queueSettings.Username);
-                         c.Password(queueSettings.Password);
-                     });
+builder.Services.AddMassTransit(mt =>
+{
+    mt.SetKebabCaseEndpointNameFormatter();
+    mt.AddMassTransit(mts =>
+        mts.UsingRabbitMq((cntx, cfg) =>
+        {
+            var queueSettings = configuration.GetSection("QueueSettings").Get<QueueSettings>();
+            cfg.Host(queueSettings.Uri, c =>
+            {
+                c.Username(queueSettings.Username);
+                c.Password(queueSettings.Password);
+            });
 
-                     cfg.SendTopology.UseCorrelationId<SubmitOrder>(x => x.OrderId);
-                 })));
+            cfg.SendTopology.UseCorrelationId<SubmitOrder>(x => x.OrderId);
+        }));
+});
 
 var app = builder.Build();
 
