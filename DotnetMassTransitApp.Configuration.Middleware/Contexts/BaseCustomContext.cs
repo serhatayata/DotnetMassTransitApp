2143ed9fa6248289aad6db6a4864687c61a09128
Serhat <srhtayata@gmail.com>
2024-05-16 20:45:11 +0300

    Pipes - Middleware filters init

---
+++ DotnetMassTransitApp.Configuration.Middleware/Contexts/BaseCustomContext.cs
@@ -0,0 +1,10 @@
+﻿using MassTransit.Middleware;
+
+namespace DotnetMassTransitApp.Configuration.Middleware.Contexts;
+
+public class BaseCustomContext :
+    BasePipeContext,
+    CustomContext
+{
+    public string SomeThing { get; set; }
+}
---
+++ DotnetMassTransitApp.Configuration.Middleware/Contexts/CustomContext.cs
@@ -0,0 +1,8 @@
+﻿using MassTransit;
+
+namespace DotnetMassTransitApp.Configuration.Middleware.Contexts;
+
+public interface CustomContext : PipeContext
+{
+    string SomeThing { get; }
+}
